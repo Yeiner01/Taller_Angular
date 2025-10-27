3bcd544929bb58c4b19b9bf162e35895635f14cf
Yeiner Ibarguen <y.ibarguenm@uniandes.edu.co>
2025-10-27 15:54:08 -0500

    ya

--- series/src/app/serie/serie-list/serie-list.ts
+++ series/src/app/serie/serie-list/serie-list.ts
@@ -3,7 +3,6 @@ import { Serie } from '../serie';
 import { dataSeries } from '../dataSeries';
 import { SerieService } from '../serie.service';
 
-
 @Component({
   selector: 'app-serie-list',
   standalone: false,
@@ -12,17 +11,36 @@ import { SerieService } from '../serie.service';
 })
 export class SerieList implements OnInit {
   series: Array<Serie> = [];
+  selectedSerie: Serie | null = null;
 
   constructor(private serieService: SerieService) { }
+  
   getSeriesList() {
     this.serieService.getSeries().subscribe(series => {
       console.log(series);
       this.series = series;
     });
-
   }
+
+
+  getAverageSeasons(): number {
+    if (this.series.length === 0) return 0;
+    
+    const totalSeasons = this.series.reduce((sum, serie) => sum + serie.seasons, 0);
+    return Math.round((totalSeasons / this.series.length) * 10) / 10;
+  }
+
+
+  selectSerie(serie: Serie): void {
+    this.selectedSerie = serie;
+  }
+
+ 
+  clearSelection(): void {
+    this.selectedSerie = null;
+  }
+
   ngOnInit(): void {
     this.getSeriesList();
-
   }
 }
